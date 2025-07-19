bfd0a3ac3168fe7bd5ff1f7722eef0da52f60cf0
omerawwad <aomerashraf22@gmail.com>
2025-07-19 18:46:16 +0300

    adding files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+import pymysql
+from datetime import datetime
+
+app = Flask(__name__)
+
+conn = pymysql.connect(host='localhost', user='root', password='yourpassword', db='notesdb')
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        note = request.form['note']
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO notes (note, timestamp) VALUES (%s, %s)", (note, timestamp))
+            conn.commit()
+    with conn.cursor() as cur:
+        cur.execute("SELECT note, timestamp FROM notes ORDER BY id DESC")
+        notes = cur.fetchall()
+    return render_template('index.html', notes=notes)
